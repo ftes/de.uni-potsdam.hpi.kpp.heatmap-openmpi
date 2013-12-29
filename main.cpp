0a75e4bf378e27d41b196f71cee37682460bfb76
Fredrik Teschke <f@ftes.de>
2013-12-29 18:42:19 +0100

    compiling, but performs wrong calcuations

--- main.cpp
+++ main.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 #include <fstream>
 #include "cl.hpp"
+#include <sstream>
+#include <math.h>
 
 #include "Hotspot.h"
 #include "Coordinate.h"
 
-/*int width;
-int height;
-int numberOfRounds;
-int currentRound = 0;
+using namespace std;
+
+
+string outFile = "output.txt";
+int width, height;
 bool writeCoords = false;
 vector<Coordinate> coords;
-vector<Hotspot> hotspots;
-vector<vector<vector<Coordinate>>> *ranges;
-
-string outFile = "output.txt";*/
-
-using namespace std;
 
 
 // OpenCL functions
@@ -88,12 +85,109 @@ void listDevices()
 
 
 // Heatmap functions
+vector<vector<int >> parseCsv(string fileName) {
+    ifstream file(fileName.c_str());
+    vector < vector<int >> result;
+    string line;
+    //ignore first line
+    getline(file, line);
+    while (getline(file, line)) {
+        vector<int> items;
+        stringstream ss(line);
+        int i;
+        while (ss >> i) {
+            items.push_back(i);
+            if (ss.peek() == ',') ss.ignore();
+        }
+        result.push_back(items);
+    }
+    file.close();
+    return result;
+}
 
-int main()
+string getOutputValue(double cell) {
+    string value;
+    if (cell > 0.9) {
+        value = "X";
+    } else {
+        value = to_string((int) floor((cell + 0.09) * 10));
+    }
+    return value;
+}
+
+void printHeatmap(vector<vector<double >> *heatmap) {
+    for (vector<double> row : *heatmap) {
+        for (double cell : row) {
+            printf("%s ", getOutputValue(cell).c_str());
+        }
+        printf("\n");
+    }
+}
+
+void writeOutput(float data[]) {
+    remove(outFile.c_str());
+    ofstream output(outFile.c_str());
+    if (!writeCoords) {
+        for (int j=0; j<height; j++) {
+            for (int i=0; i<width; i++) {
+            	string out = getOutputValue(data[i + j * width]);
+                output << out.c_str();
+
+                if (i == width - 1) {
+                	output << "\n";
+                }
+            }
+        }
+    } else {
+        for (Coordinate coord : coords) {
+            double cell = data[coord.x + coord.y * width];
+            output << cell <<"\n";
+        }
+    }
+    output.close();
+}
+
+
+
+
+int main(int argc, char* argv[])
 {
     //TODO set hotspots
+    //read input
+    width = atoi(argv[1]);
+    height = atoi(argv[2]);
+    int numberOfRounds = atoi(argv[3]);
+
+    string hotspotsFile = string(argv[4]);
+
+    if (argc == 6) {
+        string coordFile = string(argv[5]);
+        for (vector<int> line : parseCsv(coordFile)) {
+            Coordinate coord(line.at(0), line.at(1));
+            coords.push_back(coord);
+        }
+        writeCoords = true;
+    }
+
+    unsigned int hotspotsStartData[width * height];
+    unsigned int hotspotsEndData[width * height];
+    float startData[width * height];
+
+    //read hotspots
+    for (vector<int> line : parseCsv(hotspotsFile)) {
+        Hotspot hotspot(line.at(0), line.at(1), line.at(2), line.at(3));
+        int index = hotspot.x + hotspot.y * width;
+        hotspotsStartData[index] = hotspot.startRound;
+        hotspotsEndData[index] = hotspot.endRound;
+        if (hotspot.startRound == 0 && hotspot.endRound > 0) {
+            startData[index] = 1.f;
+        }
+    }
 
 
+
+
+    //OpenCL preparations
     //listDevices();
 
     //get device
@@ -120,26 +214,20 @@ int main()
         exit(1);
     }
 
-    int width = 2;
-    int height = 2;
-    float data[] = {0,1,0,1};
-    unsigned int hotspotsStartData[] = {0, 0, 0, 0};
-    unsigned int hotspotsEndData[] = {2,0,0,0};
-
     // Create an OpenCL Image for the hotspots, shall be copied to device
-    cl::Image2D hotspotsStart = cl::Image2D(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+    cl::Image2D hotspotsStartImage = cl::Image2D(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
         cl::ImageFormat(CL_R, CL_UNSIGNED_INT32), width, height, 0, hotspotsStartData);
 
     // Create an OpenCL Image for the hotspots, shall be copied to device
-    cl::Image2D hotspotsEnd = cl::Image2D(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+    cl::Image2D hotspotsEndImage = cl::Image2D(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
         cl::ImageFormat(CL_R, CL_UNSIGNED_INT32), width, height, 0, hotspotsEndData);
 
     // Create an OpenCL Image for the input data, shall be copied to device
-    cl::Image2D in = cl::Image2D(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
-        cl::ImageFormat(CL_R, CL_FLOAT), width, height, 0, data);
+    cl::Image2D startImage = cl::Image2D(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
+        cl::ImageFormat(CL_R, CL_FLOAT), width, height, 0, startData);
 
     // Create an OpenCL Image for the output data
-    cl::Image2D out = cl::Image2D(context, CL_MEM_READ_WRITE,
+    cl::Image2D otherStartImage = cl::Image2D(context, CL_MEM_READ_WRITE,
         cl::ImageFormat(CL_R, CL_FLOAT), width, height, 0);
 
     //create queue to which we will push commands for the device.
@@ -150,17 +238,30 @@ int main()
     //ranges: global offset, global (global number of work items), local (number of work items per work group)
     cl::EnqueueArgs eargs(queue,cl::NullRange,cl::NDRange(width, height), cl::NullRange);
 
-    cl::Image2D *oldH = &in;
-    cl::Image2D *newH = &out;
-    kernel(eargs, 1, hotspotsStart, hotspotsEnd, *oldH, *newH).wait();
+    // assign "wrong way round" because they are swapped back again in the for loop
+    cl::Image2D *oldHeatmapImage = &otherStartImage;
+    cl::Image2D *newHeatmapImage = &startImage;
 
-    //read output image back from device
+
+
+    //perform rounds
+    for (int i=0; i<numberOfRounds; i++) {
+        //swap heatmaps
+        cl::Image2D *tmp = oldHeatmapImage;
+        oldHeatmapImage = newHeatmapImage;
+        newHeatmapImage = tmp;
+
+        //run kernel
+        kernel(eargs, 1, hotspotsStartImage, hotspotsEndImage, *oldHeatmapImage, *newHeatmapImage).wait();
+    }
+
+    //read output back from device
     cl::size_t<3> origin, region;
     origin[0] = 0; origin[1] = 0; origin[2] = 0;
     region[0] = width; region[1] = height; region[2] = 1;
-    queue.enqueueReadImage(*newH, true, origin, region, 0, 0, data);
+    queue.enqueueReadImage(*newHeatmapImage, true, origin, region, 0, 0, startData);
 
-    cout << data[0] << "\n";
+    writeOutput(startData);
 
-    return 0;
+    exit(0);
 }
